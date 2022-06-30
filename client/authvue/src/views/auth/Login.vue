69dee02fd7def5f3e262e9df627035688fa2a361
irhamrizaldy <43570194+irhamrizaldy@users.noreply.github.com>
2022-06-30 11:13:46 +0700

    create auth views

---
+++ client/authvue/src/views/auth/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>LOGIN</h1>
+  </div>
+</template>
+<script>
+export default { name: "authLogin" };
+</script>
---
+++ client/authvue/src/views/dashboard/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>DASHBOARD</h1>
+  </div>
+</template>
+<script>
+export default { name: "dashIndex" };
+</script>
